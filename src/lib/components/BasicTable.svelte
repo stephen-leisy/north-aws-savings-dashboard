7b2b7c4d1136069d27edc371af14faf7f2b72335
Stephen Leisy <stleisy@gmail.com>
2025-03-18 11:09:30 -0700

    Search feild on table.

--- src/lib/components/BasicTable.svelte
+++ src/lib/components/BasicTable.svelte
@@ -4,12 +4,27 @@
 
 	export let tableData: DetailedSavings[];
 
+	let searchQuery: string = '';
+	let filteredData = tableData;
+
 	const tableHeaders: string[] = Object.keys(tableData[0]);
+
+	$: filteredData = tableData.filter((item) => {
+		return Object.values(item).some((value) =>
+			value.toString().toLowerCase().includes(searchQuery.toLowerCase())
+		);
+	});
 </script>
 
 <div class="w-full rounded-2xl border-1 border-[#3E404D]">
-	<header class="border-b-1 border-[#3E404D] p-4 md:p-8">
+	<header class="flex items-center justify-between border-b-1 border-[#3E404D] p-4 md:p-8">
 		<SectionHeader text="Detailed Savings" />
+		<input
+			type="text"
+			placeholder="Search Here"
+			bind:value={searchQuery}
+			class="rounded-md border border-[#3E404D] p-2 focus:border-blue-500 focus:outline-none"
+		/>
 	</header>
 
 	<table class="w-full">
@@ -21,7 +36,7 @@
 			</tr>
 		</thead>
 		<tbody class="w-full">
-			{#each tableData as { date, service, amount }}
+			{#each filteredData as { date, service, amount }}
 				<tr class="w-full border-b border-[#3E404D] last:border-b-0">
 					<td class="font-satoshi px-4 py-5 md:px-8">{date}</td>
 					<td class="font-satoshi px-4 py-5 md:px-8">{service}</td>
